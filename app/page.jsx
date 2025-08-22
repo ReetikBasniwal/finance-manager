950190fb045ad5767b8452ab1684c9a0f88e05b5
Reetik Basniwal <reetikbasniwal@gmail.com>
2025-08-22 22:14:51 +0530

    add unoptimized attribute for testinomial

--- app/page.jsx
+++ app/page.jsx
@@ -81,6 +81,7 @@ export default function Home() {
                         alt={testimonial.name} 
                         width={40} height={40} 
                         className="rounded-full" 
+                        unoptimized
                       />
                       <div className="ml-4">
                         <div className="font-semibold">{testimonial.name}</div>
